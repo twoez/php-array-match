19be8c54c546b9271d3af475fd56b9b1ed7a138c
Roy <1826350+twoez@users.noreply.github.com>
2021-02-09 17:03:07 +0100

    Create ArrayMatch.php

---
+++ ArrayMatch.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Core;
+
+/**
+ * Class ArrayMatch
+ * @package App\Core
+ */
+abstract class ArrayMatch
+{
+    /**
+     * @param string $pattern
+     * @param array $array
+     * @param array|null $matches
+     * @return mixed|null
+     */
+    public static function match(string $pattern, array $array, array &$matches = null)
+    {
+        $matches = null;
+        $parts = explode('.', $pattern);
+
+        self::_match($parts, $array, $matches);
+
+        if ($matches) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $path
+     * @param $replaceWith
+     * @param $array
+     */
+    public static function replace(array $path, $replaceWith, &$array)
+    {
+        $currentKey = current($path);
+        $nextKey = next($path);
+
+        if (false === $nextKey) {
+            $array[$currentKey] = $replaceWith;
+        } else {
+            self::replace($path, $replaceWith, $array[$currentKey]);
+        }
+    }
+
+    /**
+     * @param array $parts
+     * @param array $data
+     * @param array|null $matches
+     * @param array $track
+     * @param int $level
+     * @return false
+     */
+    private static function _match(array $parts, array $data, array &$matches = null, &$track = [], $level = 0)
+    {
+        $currentPart = current($parts);
+        $nextPart = next($parts);
+
+        foreach ($data as $key => $value) {
+            $track = array_slice($track, 0, $level);
+            $track[] = $key;
+
+            if ($nextPart && is_array($value)) {
+                if ($currentPart === '*') {
+                    if (array_key_exists($nextPart, $value)) {
+                        array_shift($parts);
+                    } else {
+                        reset($parts);
+                    }
+
+                    self::_match($parts, $value, $matches, $track, $level + 1);
+                } elseif ($currentPart === $key) {
+                    array_shift($parts);
+                    self::_match($parts, $value, $matches, $track, $level + 1);
+                }
+            } elseif (!$nextPart && ($key === $currentPart || $currentPart === '*')) {
+                $matches[] = $track;
+            }
+        }
+
+        return false;
+    }
+}
